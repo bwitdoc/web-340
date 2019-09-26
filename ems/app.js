a7cab53a8530b70634ab0f6b3c5461732e0f5dfa
Brittany Dockter <bwitdoc@gmail.com>
2019-09-25 18:48:08 -0700

    updating

--- ems/app.js
+++ ems/app.js
@@ -1,22 +1,81 @@
+/*
+=======================================
+; Title:  app.js
+; Author: Brittany Dockter
+; Date:   25 September 2019
+; Description: app.js
+;======================================
+*/
+
 var express = require("express");
 var http = require("http");
 var path = require("path");
 var logger = require("morgan");
+var mongoose = require("mongoose");
+var bodyParser = require("body-parser");
+var helmet = require("helmet");
+var cookieParser = require("cookie-parser");
+var csurf = require("csurf");
+var csrfProtection = csrf({cookie: true});
+
+var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1-pohh3.mongodb.net/test?retryWrites=true&w=majority";
+
+mongoose.connect(mongoDB, {
+    userMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error: "));
+
+db.once("open", function() {
+    console.log("Application connected to mLab");
+});
+
+// Setup CSRF Protection
+var csrfProtection = csrf({cookie: true});
+
+// Initialize Express Application
 var app = express();
 
+// Use Statements
+app.use(logger("short"));
+app.use(helmet.xssFilter());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(cookieParser());
+app.use(csrfProtection);
+app.use(function(request, response, next) {
+    var token = request.csrfToken();
+    response.cookie("XSRF-TOKEN", token);
+    response.locals.csrfToken = token;
+    next();
+});
+
+// Set Statements
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-app.use(logger("short"));
 
+
+// Routing
 app.get("/", function(request, response) {
     response.render("index", {
-        title: "Home Page"
+        message: "XSS Prevention Example"
+    });
+});
+app.get("/", function(request, response) {
+    response.render("index", {
+        message: "New Employee Entry Page"
     });
 });
 
-// mLab connection
-var mongoDB = "mongodb+srv://admin:admin@buwebdev-cluster-1-pohh3.mongodb.net/test?retryWrites=true&w=majority"
+app.post("/process", function(request, response) {
+    console.log(request.body.txtName);
+    response.redirect("/");
+});
 
+// Start Server
 http.createServer(app).listen(8080, function() {
     console.log("Application started on port 8080!");
 });
