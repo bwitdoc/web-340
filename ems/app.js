4199aafc013e1168e59cd2e638eb6e48d226212a
Brittany Dockter <bwitdoc@gmail.com>
2019-10-05 10:52:08 -0700

    updating week 9 assignments

--- ems/app.js
+++ ems/app.js
@@ -56,7 +56,7 @@ app.use(function(request, response, next) {
 // Set Statements
 app.set("views", path.resolve(__dirname, "views"));
 app.set("view engine", "ejs");
-
+app.set("port", process.env.PORT || 8080);
 
 // Routing
 app.get("/", function(request, response) {
@@ -126,6 +126,6 @@ app.post("/process", function(request, response) {
 });
 
 // Start Server
-http.createServer(app).listen(8080, function() {
-    console.log("Application started on port 8080!");
+http.createServer(app).listen(app.get("port"), function () {
+    console.log("Application started on port" + app.get("port"));
 });
